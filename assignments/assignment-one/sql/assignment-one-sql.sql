9eccf0bb0f1b32d02fd26166424d74ede8f82e64
ColeWS <73923601+ColeWS@users.noreply.github.com>
2025-01-29 15:34:05 -0500

    Delete assignments/assignment-one directory

--- assignments/assignment-one/sql/assignment-one-sql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE employees (
-    id int NOT NULL AUTO_INCREMENT,
-    firstName varchar(50) NOT NULL,
-    lastName varchar(100) NOT NULL,
-    email varchar(50) NOT NULL,
-    hoursWorked int NOT NULL,
-    primary key (id)
-);
